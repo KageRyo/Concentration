1385e48e7bbb3d5c67793860d3b3b6f0a24f3503
影凌 <k6.developer@gmail.com>
2023-03-30 14:21:28 +0800

    add: comments
    Add Chinese comments.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const cards = document.querySelectorAll('.memory-card');
 
+// 初始設定
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
@@ -8,12 +9,15 @@ function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
   this.classList.add('flip');
+
   if(!hasFlippedCard) {
-    // first click
+    // 第一次點擊
     hasFlippedCard = true;
     firstCard = this;
     return;
   }
+
+  // 第二次點擊
   secondCard = this;
   checkForMatch();
 }
@@ -21,19 +25,24 @@ function flipCard() {
 function checkForMatch() {
   const firstCardAlt = firstCard.querySelector('.front-face').alt;
   const secondCardAlt = secondCard.querySelector('.front-face').alt;
+
+  // 檢查是否匹配
   let isMatch = firstCardAlt === secondCardAlt;
   isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
+  // 匹配成功，移除點擊事件
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
   resetBoard();
 }
 
 function unflipCards() {
+  // 鎖定遊戲版面
   lockBoard = true;
 
+  // 非匹配卡片翻回去
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
@@ -42,15 +51,18 @@ function unflipCards() {
 }
 
 function resetBoard() {
+  // 重置遊戲變數
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
 }
 
 (function shuffle() {
+  // 洗牌
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * 20);
     card.style.order = randomPos;
   });
 })();
 
+// 為每張卡片添加點擊事件
 cards.forEach(card => card.addEventListener('click', flipCard));
