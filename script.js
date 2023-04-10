a76c546dccfda3eaf5c4713da4f4d273f68c6c1d
Ryo <k6.developer@gmail.com>
2023-04-10 14:01:17 +0800

    完成到聆聽難易度按鈕的部分
    Co-Authored-By: RRAaru <100890852+RRAaru@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
+// 卡片
 const cards = document.querySelectorAll('.show');
+// 翻牌
 const flip = document.querySelectorAll('.memory-card');
+// 難易度
+const easy = document.querySelectorAll('#easy');
+const normal = document.querySelectorAll('#normal');
+const hard = document.querySelectorAll('#hard');
 
 // 初始設定
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let difficulty = normal;
 
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
   this.classList.add('flip');
-
   if(!hasFlippedCard) {
     // 第一次點擊
     hasFlippedCard = true;
     firstCard = this;
     return;
   }
-
   // 第二次點擊
   secondCard = this;
   checkForMatch();
@@ -26,7 +31,6 @@ function flipCard() {
 function checkForMatch() {
   const firstCardAlt = firstCard.querySelector('.front-face').alt;
   const secondCardAlt = secondCard.querySelector('.front-face').alt;
-
   // 檢查是否匹配
   let isMatch = firstCardAlt === secondCardAlt;
   isMatch ? disableCards() : unflipCards();
@@ -44,13 +48,18 @@ function disableCards() {
 function unflipCards() {
   // 鎖定遊戲版面
   lockBoard = true;
-
   // 非匹配卡片翻回去
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
     resetBoard();
   }, 500);
+}
+
+function difficultyChoose() {
+  // 難易度選擇
+  console.log("蛤");
+
 }
 
 function resetBoard() {
@@ -69,3 +78,7 @@ function resetBoard() {
 
 // 為每張卡片添加點擊事件
 flip.forEach(card => card.addEventListener('click', flipCard));
+// 難易度的點擊事件
+easy.forEach(easy => easy.addEventListener('click', difficultyChoose));
+normal.forEach(normal => normal.addEventListener('click', difficultyChoose));
+hard.forEach(hard => hard.addEventListener('click', difficultyChoose));
